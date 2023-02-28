77417abcd76630e174dce8e09e4526e4171af1b1
rajuswami123 <raju.swamireddy@tringapps.com>
2023-02-28 10:05:03 +0530

    intial commit

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,41 @@
+package org.example;
+import java.util.*;
+import java.util.logging.Logger;
+class Student{
+    private String name;
+    private int age;
+    private double gpa;
+    public Student(String name, int age, double gpa) {
+        super();
+        this.name = name;
+        this.age = age;
+        this.gpa = gpa;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public double getGpa() {
+        return gpa;
+    }
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentTest [toString()=" + super.toString() + "]";
+    }
+
+}
+
+
+
